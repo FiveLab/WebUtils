0e25de7ef674948f8dc699682aad82ad53f3b4e9
Vitalii Zhuk <zhuk2205@gmail.com>
2025-12-19 14:04:44 +0200

    Add rollup bundler

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+
+export default [
+    {
+        input: 'src/index.ts',
+        output: {
+            dir: 'dist',
+            format: 'esm',
+            preserveModules: true,
+            preserveModulesRoot: 'src',
+            sourcemap: true
+        },
+        plugins: [
+            resolve({
+                browser: true,
+                extensions: ['.ts'],
+            }),
+            typescript({
+                tsconfig: './tsconfig.json',
+                declaration: false,
+            }),
+        ],
+    }
+];
