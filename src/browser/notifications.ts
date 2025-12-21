f56f9249b7ce7d5001c4bacd3a30d74ae1713669
Vitalii Zhuk <zhuk2205@gmail.com>
2025-12-21 15:24:03 +0200

    Add notification helpers

--- src/browser/notifications.ts
+++ src/browser/notifications.ts
@@ -18,3 +18,20 @@ export async function notifyInfo(message: string) {
 export async function notifyWarning(message: string) {
     await getAdapter().warning(message);
 }
+
+export async function notifyFetchError() {
+    await getAdapter().error('Network error: unable to fetch the API request.');
+}
+
+export async function notifyByResponse(response: Response): Promise<void>;
+export async function notifyByResponse(response: Response, message: string): Promise<void>
+
+export async function notifyByResponse(response: Response, message?: string) {
+    if ([200, 201, 204].includes(response.status)) {
+        if (message) {
+            await notifySuccess(message);
+        }
+    } else {
+        await notifyError(`Receive wrong status - ${response.statusText}`);
+    }
+}
--- tests/browser/notifications.test.ts
+++ tests/browser/notifications.test.ts
@@ -1,6 +1,6 @@
 import { afterEach, describe, expect, it, Mock, vi } from 'vitest';
 import { getAdapter } from '../../src/browser/notifications/adapter';
-import { notifyError, notifyInfo, notifySuccess, notifyWarning } from '../../src/browser';
+import { notifyByResponse, notifyError, notifyFetchError, notifyInfo, notifySuccess, notifyWarning } from '../../src/browser';
 
 const createAdapter = () => {
     const success = vi.fn().mockResolvedValue(undefined);
@@ -79,5 +79,50 @@ describe('notify error', () => {
         expect(getAdapter).toHaveBeenCalledOnce();
         expect(error).toHaveBeenCalledOnce();
         expect(error).toHaveBeenCalledWith('some error');
+    });
+});
+
+describe('notify fetch error', () => {
+    it('notify', async () => {
+        const {getAdapter, error} = createAdapter();
+
+        await notifyFetchError();
+
+        expect(getAdapter).toHaveBeenCalledOnce();
+        expect(error).toHaveBeenCalledOnce();
+        expect(error).toHaveBeenCalledWith('Network error: unable to fetch the API request.');
+    });
+});
+
+describe('notify by response', () => {
+    it.each([
+        [200],
+        [201],
+        [204]
+    ])('success status - %d', async (status) => {
+        const {getAdapter, success} = createAdapter();
+
+        await notifyByResponse(new Response(null, {status: status}), 'some foo bar');
+
+        expect(getAdapter).toHaveBeenCalledOnce();
+        expect(success).toHaveBeenCalledWith('some foo bar');
+    });
+
+    it('success without text', async () => {
+        const {getAdapter, success} = createAdapter();
+
+        await notifyByResponse(new Response(null, {status: 200}));
+
+        expect(getAdapter).not.toBeCalled();
+        expect(success).not.toBeCalled();
+    });
+
+    it('wrong status', async () => {
+        const {getAdapter, error} = createAdapter();
+
+        await notifyByResponse(new Response(null, {status: 500, statusText: 'Internal Server Error'}));
+
+        expect(getAdapter).toHaveBeenCalledOnce();
+        expect(error).toHaveBeenCalledWith('Receive wrong status - Internal Server Error');
     });
 });
